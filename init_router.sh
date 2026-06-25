faaf69470f413de0c5cc0e8bc584c013d80925ea
vargasol <66957125+vargasol@users.noreply.github.com>
2026-06-25 15:10:04 +0200

    Update init_router.sh

--- init_router.sh
+++ init_router.sh
@@ -5,9 +5,9 @@ ip route del default
 
 echo "Searching internet gateway..."
 IP=$(route -n |grep 'U[ \t].*eth1' | awk '{print $1}')
-IP_HEX=$(printf '%.2X%.2X%.2X%.2X\n' `echo $IP | sed -e 's/\./ /g'`)
-NEXT_IP_HEX=$(printf %.8X `echo $(( 0x$IP_HEX + 1 ))`)
-DEF_GW=$(printf '%d.%d.%d.%d\n' `echo $NEXT_IP_HEX | sed -r 's/(..)/0x\1 /g'`)
+LAST_OCTET="${IP##*.}"
+PREFIX="${IP%.*}"
+DEF_GW="${PREFIX}.$((LAST_OCTET + 1))"
 
 echo "Setting up internet routing via $DEF_GW..."
 ip route add 0.0.0.0/0 via $DEF_GW
@@ -18,11 +18,11 @@ apt upgrade -y
 apt install iptables-persistent traceroute net-tools mc -y
 
 echo "Setting up internal network routing..."
-IP=$(route -n | grep 'U[ \t].*eth0' | awk '{print $1}')
-IP_HEX=$(printf '%.2X%.2X%.2X%.2X\n' `echo $IP | sed -e 's/\./ /g'`)
-NEXT_IP_HEX=$(printf %.8X `echo $(( 0x$IP_HEX + 1 ))`)
-INT_GW=$(printf '%d.%d.%d.%d\n' `echo $NEXT_IP_HEX | sed -r 's/(..)/0x\1 /g'`)
-echo "Internal gateway ip addres: $INT_GW"
+
+IP=$(route -n |grep 'U[ \t].*eth0' | awk '{print $1}')
+LAST_OCTET="${IP##*.}"
+PREFIX="${IP%.*}"
+INT_GW="${PREFIX}.$((LAST_OCTET + 1))"
 
 for i in "${@}"; do
  echo "Adding range: $i"
